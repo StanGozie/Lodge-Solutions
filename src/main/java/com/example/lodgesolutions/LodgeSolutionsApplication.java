a402285d6d3b02efa8f9b91c48882570db2a8f67
StanGozie <chigozie.enyoghasi@decagon.dev>
2023-03-05 02:58:27 +0100

    first commit

---
+++ src/main/java/com/example/lodgesolutions/LodgeSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lodgesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgeSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodgeSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/lodgesolutions/models/Apartment.java
@@ -0,0 +1,2 @@
+package com.example.lodgesolutions.models;public class Apartment {
+}
